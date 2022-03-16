3db521c7368c12749d7d982cfbbe93b0694530f1
Simon Farrow <simon.farrow1@nhs.net>
2022-03-16 15:44:20 +0000

    Added Response Importer for ValidationResponse support

--- runtkwfhirvalidator.sh
+++ runtkwfhirvalidator.sh
@@ -1,7 +1,10 @@
 #Report the version numbers
-if [[ -e /home/service/TKW/config/FHIR_BaRS/version_string.txt ]]
+
+PROJECT=FHIR_BaRS
+
+if [[ -e /home/service/TKW/config/$PROJECT/version_string.txt ]]
 then
-	cat /home/service/TKW/config/FHIR_BaRS/version_string.txt
+	cat /home/service/TKW/config/$PROJECT/version_string.txt
 fi
 java -jar /home/service/TKW/TKW-x.jar -version | grep -v "starting on"
 
@@ -18,13 +21,16 @@ echo "keyStorePassword = " $keyStorePassword
 
 cd /home/service
 
+java -cp $TKWROOT/TKW-x.jar uk.nhs.digital.mait.tkwx.tk.boot.BARSResponseImporter $PROJECT &
+
 # decide whether its TLSMA or not
 if [ "$trustStore" == 'default' ]
 then
 	#ClearText
 	java -version
-	java -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_BaRS/tkw-x_provider_simulator.properties
+	java -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/tkw-x_provider_simulator.properties
 else
 	#TLSMA
-	java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_BaRS/tkw-x_provider_simulator.properties
+	java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/tkw-x_provider_simulator.properties
 fi
+
