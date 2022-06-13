91b775de6701f5731047e83837efd5251ffa0d6f
Richard Robinson <rrobinson@nhs.net>
2022-06-13 16:59:15 +0100

    Updated Postman collection, simulator updates  and docker file changes to support external configs.

--- runtkwfhirvalidator.sh
+++ runtkwfhirvalidator.sh
@@ -18,8 +18,16 @@ echo "trustStore = " $trustStore
 echo "trustStorePassword = " $trustStorePassword
 echo "keyStore = " $keyStore
 echo "keyStorePassword = " $keyStorePassword
+echo "TERMINOLOGY_SERVER_ACCESS_TOKEN_ROOT = " $TERMINOLOGY_SERVER_ACCESS_TOKEN_ROOT
 
 cd /home/service
+
+if [ -f "$TERMINOLOGY_SERVER_ACCESS_TOKEN_ROOT/fhir_terminology_server_access_token.txt" ]
+then
+	javastring="-Djava.net.preferIPv4Stack=true -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/tkw-x_provider_simulator.properties"
+else
+	javastring="-Djava.net.preferIPv4Stack=true -Duk.nhs.digital.mait.tkwx.tk.internalservices.validation.hapifhir.r4.terminologyserveraccesstokenlocation=$TERMINOLOGY_SERVER_ACCESS_TOKEN_ROOT/fhir_terminology_server_access_token.txt -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/tkw-x_provider_simulator.properties"
+fi
 
 java -cp $TKWROOT/TKW-x.jar uk.nhs.digital.mait.tkwx.tk.boot.BARSResponseImporter $PROJECT &
 
@@ -28,9 +36,11 @@ if [ "$trustStore" == 'default' ]
 then
 	#ClearText
 	java -version
-	java -Djava.net.preferIPv4Stack=true -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/tkw-x_provider_simulator.properties
+	echo java $javastring
+	java $javastring
 else
 	#TLSMA
-	java -Djava.net.preferIPv4Stack=true -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/tkw-x_provider_simulator.properties
+	java -version
+	java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword $javastring
 fi
 
