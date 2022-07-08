1df589b7f54aca48905505050c305eb47fb06eb5
Richard Robinson <rrobinson@nhs.net>
2022-07-08 16:22:43 +0100

    updates to capability statement, inhibit respons importer flag

--- runtkwfhirvalidator.sh
+++ runtkwfhirvalidator.sh
@@ -19,6 +19,7 @@ echo "trustStorePassword = " $trustStorePassword
 echo "keyStore = " $keyStore
 echo "keyStorePassword = " $keyStorePassword
 echo "TERMINOLOGY_SERVER_ACCESS_TOKEN_ROOT = " $TERMINOLOGY_SERVER_ACCESS_TOKEN_ROOT
+echo "INHIBIT_RESPONSE_IMPORTER = " $INHIBIT_RESPONSE_IMPORTER
 
 cd /home/service
 
@@ -29,7 +30,11 @@ else
 	javastring="-Djava.net.preferIPv4Stack=true -Duk.nhs.digital.mait.tkwx.tk.internalservices.validation.hapifhir.r4.terminologyserveraccesstokenlocation=$TERMINOLOGY_SERVER_ACCESS_TOKEN_ROOT/fhir_terminology_server_access_token.txt -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/tkw-x_provider_simulator.properties"
 fi
 
-java -cp $TKWROOT/TKW-x.jar uk.nhs.digital.mait.tkwx.tk.boot.BARSResponseImporter $PROJECT &
+if [ "$INHIBIT_RESPONSE_IMPORTER" != 'true' ]
+then
+	java -cp $TKWROOT/TKW-x.jar uk.nhs.digital.mait.tkwx.tk.boot.BARSResponseImporter $PROJECT &
+	echo "Starting BaRS Response Importer"
+fi
 
 # decide whether its TLSMA or not
 if [ "$trustStore" == 'default' ]
