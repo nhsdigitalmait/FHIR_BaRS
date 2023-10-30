87dd2080c3178c2a95ebbbbaeb94ec96d10f867d
sharif ullah <sharif.ullah1@nhs.net>
2023-10-30 10:33:16 +0000

    updates for application5 release chnaged to 1.0.3

--- build_provider_simulator_Docker.sh
+++ build_provider_simulator_Docker.sh
@@ -3,7 +3,7 @@
 # usage build_provider_simulator_Docker.sh [<userid>]
 # if no user id is provided it defaults to 1000 and the tag is just the version number
 #
-TAG=1.0.2
+TAG=1.0.3
 
 if [[ "$1" == "" ]]
 then
