cb02b82387175c54bb8d2e4134ddb682b16a3f4d
chbe53 <chris.berry1@nhs.net>
2025-03-05 12:52:19 +0000

    Fixes for Release 1.0.16 NBRS-4028

--- build_provider_simulator_Docker.sh
+++ build_provider_simulator_Docker.sh
@@ -3,7 +3,7 @@
 # usage build_provider_simulator_Docker.sh [<userid>]
 # if no user id is provided it defaults to 1000 and the tag is just the version number
 #
-TAG=1.0.15
+TAG=1.0.16
 if [[ "$1" == "" ]]
 then
 	USER_ID=1000
